42c0440abf6546223520fc2054e4df63305b5820
raptor <buckyballreaction@gmail.com>
2013-11-01 11:06:26 -0600

    Remove old Java logbot, it is superseded by the python bot

--- BitfighterLogBot/src/org/bitfighter/logbot/socket/SocketHandler.java
+++
@@ -1,9 +0,0 @@
-package org.bitfighter.logbot.socket;
-
-import java.net.Socket;
-
-import org.jibble.pircbot.PircBot;
-
-public abstract class SocketHandler {
-	public abstract void run(PircBot bot, Socket socket);
-}

efb4d4f5315007fab30462b2885f3c4c17e487e0
raptor <buckyballreaction@gmail.com>
2014-12-06 21:51:51 -0700

    Minor MOTD php changes

--- scripts/master_server/motd/index.php
+++ scripts/master_server/motd/index.php
@@ -12,7 +12,7 @@
 <form action="savemotd.php" name="motd" method="post">
 <h1 style="color: #e0bf07">Bitfighter MOTD</h1>
 <p>Use this form to set a new MOTD message that will be displayed when players start Bitfighter.</p>
-<p>Guidelines:
+<h3>Guidelines</h3>
 <ul>
 <li>Keep it clean
 <li>Keep is clear
@@ -20,14 +20,14 @@
 <li>Keep it factual
 <li>Don't piss off the devs
 </ul>
-</p>
-<p>Existing MOTD:</p>
+<h3>Existing MOTD</h3>
 <div id="motd" style="border: 1px solid #ccc; padding: 10px; margin-top: 10px; margin-bottom: 20px;">
 <!--#include virtual="./motd" -->
 </div>
-<p>Enter New MOTD:</p>
+<h3>Enter New MOTD</h3>
 <input type="text" name="motd" size="80" maxlength="200" />
 <p><input type="checkbox" name="twitter" value="yes" checked="checked"/>Send to Twitter</p>
+<p><b>Verify the text and options before submitting.</b></p>
 <input type="submit" value="Submit" />
 </form> 
 
